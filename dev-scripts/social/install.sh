7b6f0a2a9cdda33967a9398ed9faa372b255d0b5
jaapjan <mail@jaapjan.nl>
2016-09-20 10:15:38 +0200

    Remove some special OS scripts.

--- dev-scripts/social/install.sh
+++ dev-scripts/social/install.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-# Parse YAML, see: http://stackoverflow.com/a/21189044
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
 # Prompt for a YES.
 read -p "You will lose any existing data. Are you sure you want to (re-)install the site? [Y/n]:" -n 1 -r
 echo ""
@@ -37,14 +19,6 @@ if [[ $REPLY =~ ^[Yy]$ ]]
     PROJECT_ID=$1
     ENVIRONMENT_ID=$2
 
-
-    ##########################
-    # Social site settings.  #
-    ##########################
-
-    eval $(parse_yaml .social.settings.yml "CONFIG_")
-
-
     #########################
     # Lets do a re-install  #
     #########################
@@ -52,12 +26,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]
     # TODO This might not be necessary if we have a working basic master environment.
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'si social'
 
-
     ###############################
     # Lets generate demo content  #
     ###############################
 
-    # TODO Check for CONFIG_DEMO_CONTENT.
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'pm-enable social_demo'
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'cc drush'
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'sda file user group topic event eventenrollment post comment'
@@ -66,13 +38,5 @@ if [[ $REPLY =~ ^[Yy]$ ]]
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'queue-run activity_creator_logger'
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'queue-run activity_creator_activities'
     platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes 'php-eval "node_access_rebuild()"'
-
-
-    ######################################################
-    # Lets set some social site settings during install  #
-    ######################################################
-
-    platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes "config-set system.site name '$CONFIG_site_name'"
-    platform drush -p $PROJECT_ID -e $ENVIRONMENT_ID --yes "config-set system.site mail $CONFIG_site_mail"
 
   fi
